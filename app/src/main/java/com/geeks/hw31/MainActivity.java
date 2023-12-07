0cf1cee60d70e8906a2686470626507e8cd9a592
megodevice <megodevice14@gmail.com>
2023-12-07 23:07:15 +0600

    Initial commit

---
+++ app/src/main/java/com/geeks/hw31/MainActivity.java
@@ -0,0 +1,39 @@
+package com.geeks.hw31;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private EditText editTextEmail;
+    private EditText editTextTheme;
+    private EditText editTextText;
+    private Button buttonSend;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editTextEmail = findViewById(R.id.edit_text_email);
+        editTextTheme = findViewById(R.id.edit_text_theme);
+        editTextText = findViewById(R.id.edit_text_text);
+        buttonSend = findViewById(R.id.button_send);
+    }
+
+    public void onSendButtonClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{editTextEmail.getText().toString()});
+        intent.putExtra(Intent.EXTRA_SUBJECT, editTextTheme.getText().toString());
+        intent.putExtra(Intent.EXTRA_TEXT, editTextText.getText().toString());
+        startActivity(intent);
+    }
+}
